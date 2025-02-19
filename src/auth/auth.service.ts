49f6f04345ac431217011ed7f42101b78229c224
akmalrmn <akmalramadannn@gmail.com>
2025-02-19 16:43:32 +0700

    feat: login and register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,50 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { error } from "console";
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I am a signin route' };
+  
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Invalid credentials');
+
+    const isValid = await argon.verify(user.hash, dto.password);
+
+    if (!isValid) throw new ForbiddenException('Invalid credentials');
+    
+    return user;
   }
 
-  signup(dto: AuthDto) {
-    return { msg: 'I am a signup route' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      })
+      
+      return user;
+    }
+    catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException(
+          'This email is already in use. Please use a different email.'
+        )
+      }
+    }
+    throw error;
   }
 }
