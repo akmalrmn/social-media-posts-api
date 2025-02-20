a1fb85237a27c72d02288ca17ccb131d3ae3c9ad
akmalrmn <akmalramadannn@gmail.com>
2025-02-20 21:42:06 +0700

    feat: jwt auth

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private prisma: PrismaService) {
+    const JwtSecret = process.env.JWT_SECRET;
+    if (!JwtSecret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JwtSecret,
+    });
+  }
+
+  async validate(payload: { userId: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    delete (user as any).hash;
+
+    return user;
+  }
+}
